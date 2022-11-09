962f4397850a6af6c984baa39c6939a2a479d78f
7bellaa <96913791+7bellaa@users.noreply.github.com>
2022-11-09 17:52:02 +0900

    [Bronze I] Title: 2007년, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -1,8 +1,5 @@
 m = [31,28,31,30,31,30,31,31,30,31,30,31]
-d = ['MON','TUE','WED','THU','FRI','SAT','SUN']
+d = ['SUN','MON','TUE','WED','THU','FRI','SAT']
 a, b = map(int, input().split())
-tmp = -1
-for i in range(a-1):
-    tmp += m[i]
-tmp += b
-print(d[tmp%7])
+for i in range(a-1): b += m[i]
+print(d[b%7])
