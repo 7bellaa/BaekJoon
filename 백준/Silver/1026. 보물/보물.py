466caf3e34e1ef82c553d09079283a87e774f080
7bellaa <96913791+7bellaa@users.noreply.github.com>
2022-11-09 19:25:16 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,13 +1,7 @@
-import sys
-from heapq import *
-input = sys.stdin.readline
 n = int(input())
 a = list(map(int, input().split()))
-tmp = list(map(int, input().split()))
-b = []
-for i in tmp: heappush(b,i)
-a.sort(reverse=True)
+b = list(map(int, input().split()))
 ans = 0
-for i in a:
-    ans += i * heappop(b)
+for i,j in zip(sorted(a),sorted(b,reverse=True)):
+    ans += i*j
 print(ans)
