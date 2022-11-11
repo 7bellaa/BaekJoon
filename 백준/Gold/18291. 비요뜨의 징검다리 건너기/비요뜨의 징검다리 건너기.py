a30c26507207e8a7f5f175f3efec344faf67ef70
7bellaa <96913791+7bellaa@users.noreply.github.com>
2022-11-11 20:14:49 +0900

    [Gold V] Title: 비요뜨의 징검다리 건너기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.py
+++ 백준/Gold/18291. 비요뜨의 징검다리 건너기/비요뜨의 징검다리 건너기.py
@@ -1,18 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-def fpow(a, n, c):
-    if n == 1: return a % c
-    else:
-        x = fpow(a, n//2, c)
-        if n & 1:
-            return ((x%c) * (x%c) * (a%c)) % c
-        else:
-            return ((x%c) * (x%c)) % c
-
 T = int(input())
-c = 1000000007
 for _ in range(T):
     n = int(input())
-    if n > 2: print(fpow(2, n-2, c))
+    if n > 2: print(pow(2, n-2, 1000000007))
     else: print(1)
