df9d3d5556b797c82f9e3996791f1a7c2f172410
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-01-05 12:24:10 +0900

    [Bronze III] Title: 홀수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int a, res = 100, sum = 0;
+    for (int i = 0; i < 7; i++) {
+        cin >> a;
+        if (a & 1) {
+            res = min({res, a});
+            sum += a;
+        }
+    }
+    sum ? cout << sum << '\n' << res : cout << -1;
+    return 0;
+}
