da3cee2864576be0c67c565cac8e2f3217912f00
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-01-11 11:36:15 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int n,i,j;
+    cin >> n;
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j++) cout << '*';
+        for (j = 0; j < 2*(n-i); j++) cout << ' ';
+        for (j = 0; j < i; j++) cout << '*';
+        cout << '\n';
+    }
+    for (i = n-1; i > 0; i--) {
+        for (j = 0; j < i; j++) cout << '*';
+        for (j = 0; j < 2*(n-i); j++) cout << ' ';
+        for (j = 0; j < i; j++) cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
