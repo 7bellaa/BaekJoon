96be200c04573e5c08021091cbf828b83a8600e3
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-01-17 14:49:59 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void) {
+    int n; cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) q.push(i);
+    while (q.size() > 1) {
+        q.pop();
+        int t = q.front();
+        q.pop();
+        q.push(t);
+    }
+    cout << q.front();
+    return 0;
+}
