95e0d56af2520c5654782fda8e416b44127ff563
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-01-17 13:38:32 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int MX = 20000;
+int dat[MX], head, tail;
+
+void push(int x){
+    dat[tail++] = x;
+}
+
+void pop(){
+    head++;
+}
+
+int front(){
+    return dat[head];
+}
+
+int back(){
+    return dat[tail-1];
+}
+
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    string s;
+    int n, x;
+    cin >> n;
+    while (n--) {
+        cin >> s;
+        if (s == "push") {
+            cin >> x;
+            push(x);
+        }
+        else if (s == "pop") {
+            if (head == tail) cout << "-1\n";
+            else {cout << front() << '\n'; pop();}
+        }
+        else if (s == "size") {
+            cout << tail-head << '\n';
+        }
+        else if (s == "empty") {
+            cout << !(tail-head) << '\n';
+        }
+        else if (s == "front") {
+            if (head == tail) cout << "-1\n";
+            else cout << front() << '\n';
+        }
+        else {
+            if (head == tail) cout << "-1\n";
+            else cout << back() << '\n';
+        }
+    }
+    return 0;
+}
