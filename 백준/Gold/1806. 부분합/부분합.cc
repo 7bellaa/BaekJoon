3215f07074c05595c5d9b6ccbe7433f975d866aa
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-04-19 02:52:45 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int N, S;
+    cin >> N >> S;
+    vector<int> v(N+1,0);
+    for (int i = 1; i <= N; i++) {
+        cin >> v[i];
+        v[i] += v[i-1];
+    }
+
+    int start = 0, end = 1;
+    int cmp = N+1;
+
+    while (end <= N) {
+        if (v[end] - v[start] < S) {end++; continue;}
+        while (v[end] - v[start] >= S) start++;
+        if (end - start + 1 < cmp) cmp = end - start + 1;
+        end++;
+        start--;
+    }
+    if (cmp == N+1) cout << 0;
+    else cout << cmp;
+}
