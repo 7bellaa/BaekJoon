36cce44afeee3830c0e260858c5a537fb5a5fcfe
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-04-20 23:20:30 +0900

    [Silver III] Title: 칸토어 집합, Time: 4 ms, Memory: 4260 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> dp(13, "\0");
+
+string cantor(const long long &n) {
+    if (dp[n] != "\0") return dp[n];
+    string tmp1 = cantor(n-1), tmp2 = "";
+    for (int i = 0; i < tmp1.length(); i++) tmp2 += " ";
+    return dp[n] = tmp1 + tmp2 + tmp1;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    long long n;
+    dp[0] = "-";
+    while (cin >> n) cout << cantor(n) << '\n';
+}
