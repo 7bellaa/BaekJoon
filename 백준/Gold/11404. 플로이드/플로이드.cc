54c09f76c5ea157f6fd90f1e379981df24699b9b
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-05-14 01:56:24 +0900

    [Gold IV] Title: 플로이드, Time: 24 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,35 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N_MAX = numeric_limits<int>::max();
-long long board[101][101];
-long long N, M, a, b, c;
+int board[101][101];
+int N, M, a, b, c;
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    fill(&board[0][0], &board[100][101], N_MAX);
+    fill(&board[0][0], &board[100][101], -1);
     cin >> N >> M;
     while (M--) {
         cin >> a >> b >> c;
-        board[a][b] = min(board[a][b], c);
+        if (board[a][b] == -1) board[a][b] = c;
+        else board[a][b] = min(board[a][b], c);
     }
     for (int i = 1; i <= N; i++) board[i][i] = 0;
 
     for (int k = 1; k <= N; k++) {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (board[i][k] != N_MAX && board[k][j] != N_MAX) board[i][j] = min(board[i][j], board[i][k] + board[k][j]);
+                if (board[i][k] == -1 || board[k][j] == -1) continue;
+                if (board[i][j] == -1) board[i][j] = board[i][k] + board[k][j]; 
+                else board[i][j] = min(board[i][j], board[i][k] + board[k][j]);
             }
         }
     }
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            if (board[i][j] == N_MAX) cout << 0 << ' ';
+            if (board[i][j] == -1) cout << 0 << ' ';
             else cout << board[i][j] << ' ';
         }
         cout << '\n';
     }
 }
-
-// 간선이 없는 경우 0을 출력해야하는데 처리 안해서 틀림
