82bf961bc97621c9379597083950fc06a9c295e1
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-09-07 18:03:22 +0900

    [Gold II] Title: 이전 수열은 어떤 수열일까, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1898. 이전 수열은 어떤 수열일까/이전 수열은 어떤 수열일까.cc
+++ 백준/Gold/1898. 이전 수열은 어떤 수열일까/이전 수열은 어떤 수열일까.cc
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> v(50001);
+vector<int> vis(50001);
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
-    vector<int> v(n);
-    vector<bool> vis(n, false);
-    for (auto &i : v) cin >> i;
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+        vis[v[i]] = i;
+    }
 
     for (int i = 0; i < n; i++) {
         int cur = v[i];
-        if (vis[cur] || vis[cur-1] || cur == 1) continue;
-        auto it1 = v.begin() + i, it2 = find(v.begin(), v.end(), cur-1);
-        if (it1 < it2) {
-            vis[cur] = vis[cur-1] = true;
-            iter_swap(it1, it2);
+        if (vis[cur] == -1 || vis[cur-1] == -1 || cur == 1) continue;
+        if (vis[cur] < vis[cur-1]) {
+            iter_swap(v.begin() + vis[cur], v.begin() + vis[cur-1]);
+            vis[cur] = vis[cur-1] = -1;
         }
     }
-    for (auto i : v) cout << i << '\n';
+    for (int i = 0; i < n; i++) cout << v[i] << ' ';
 }
