ccc621affba4dfdf90eb61f58ca5a9c31cf385fc
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-10-13 00:54:32 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> nums;
+
+void solve(int x) {
+    if (x == M) {
+        for (auto i : nums) cout << i << ' ';
+        cout << '\n';
+        return ;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (!nums.empty() && i < nums.back()) continue;
+        nums.push_back(i);
+        solve(x+1);
+        nums.pop_back();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> M;
+    solve(0);
+}
