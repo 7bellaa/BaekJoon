74cf2a2c3faf856059a22ea1b039149b6aeae48a
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-10-23 19:08:39 +0900

    [Bronze II] Title: 잉여역수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15995. 잉여역수 구하기/잉여역수 구하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int a, m; cin >> a >> m;
+    for (int i = 1; i < m; i++) {
+        if ((a * i) % m == 1) {
+            cout << i;
+            return 0;
+        }
+    }
+}
