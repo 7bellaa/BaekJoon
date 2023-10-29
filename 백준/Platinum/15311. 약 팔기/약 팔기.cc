eba79b05775629832047456d27ce79d7d0c17b65
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-10-30 01:19:06 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin >> n;
+    cout << 1999 << '\n';
+    for (int i = 0; i < 1000; i++) cout << 1000 << ' ';
+    for (int i = 0; i < 999; i++) cout << 1 << ' ';
+}
