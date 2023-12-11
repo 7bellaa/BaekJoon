56622ccf8e438b0fe0107cd285907921fe429cde
7bellaa <96913791+7bellaa@users.noreply.github.com>
2023-12-11 15:35:52 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 3684 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, t;
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    vector<int> num;
-    int n; cin >> n;
+    vector<int> num, dp;
+    
+    cin >> n;
     for (int i = 0; i < n; i++) {
-        int t; cin >> t;
+        cin >> t;
         num.push_back(t);
     }
 
     int ans = num[0];
-    vector<int> dp;
     dp.push_back(num[0]);
 
     for (int i = 1; i < n; i++) {
-        if (num[i] > num[i] + dp.back()) dp.push_back(num[i]);
-        else dp.push_back(dp.back() + num[i]);
+        dp.push_back(max(num[i], dp.back() + num[i]));
         ans = max(ans, dp.back());
     }
 
