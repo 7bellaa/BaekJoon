dde97696af44f823ff06c7ecca74bb3c17ff76a7
정선웅 <96913791+7bellaa@users.noreply.github.com>
2026-03-10 13:42:25 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.cc
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.cc
@@ -1,23 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, L, H, ans, cmp, height[1001];
+
+int n, height[1001];
+
+void solve() {
+  int left = 1, right = 1000, left_max = 0, right_max = 0, area = 0;
+  while (left <= right) {
+    left_max = max(left_max, height[left]);
+    right_max = max(right_max, height[right]);
+
+    if (left_max < right_max) {
+      area += left_max;
+      left++;
+    } else {
+      area += right_max;
+      right--;
+    }
+  }
+  cout << area;
+}
 
 int main() {
-    cin >> N;
-    while (N--) {
-        cin >> L >> H;
-        height[L] = H;
-    }
-    
-    int topIdx = distance(height, max_element(height, height+1001));
-    for (int i = 0; i <= topIdx; i++) {
-        cmp = max(cmp, height[i]);
-        ans += cmp;
-    }
-    cmp = 0;
-    for (int i = 1000; i > topIdx; i--) {
-        cmp = max(cmp, height[i]);
-        ans += cmp;
-    }
-    cout << ans;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> n;
+
+  int l, h;
+  for (int i = 0; i < n; i++) {
+    cin >> l >> h;
+    height[l] = h;
+  }
+
+  solve();
 }
