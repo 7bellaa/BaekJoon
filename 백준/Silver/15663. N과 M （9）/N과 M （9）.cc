7e4f1434207b37447856fccf84fddde177d369d6
정선웅 <96913791+7bellaa@users.noreply.github.com>
2026-03-18 15:19:13 +0900

    [Silver II] Title: N과 M (9), Time: 28 ms, Memory: 3612 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int n, m, arr[9], ans[9];
+bool vis[9];
+set<string> S;
+
+void f(int k) {
+  if (k == m) {
+    string str = "";
+    for (int i = 0; i < m; i++)
+      str += to_string(ans[i]) + " ";
+
+    if (S.find(str) != S.end())
+      return;
+    S.insert(str);
+    cout << str << '\n';
+    return;
+  }
+
+  for (int i = 0; i < n; i++) {
+    if (vis[i])
+      continue;
+    vis[i] = true;
+    ans[k] = arr[i];
+    f(k + 1);
+    vis[i] = false;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+  sort(arr, arr + n);
+  f(0);
+}
