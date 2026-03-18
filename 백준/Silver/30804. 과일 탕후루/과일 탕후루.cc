1a824a6132de938b9554b1664278a3dede973278
정선웅 <96913791+7bellaa@users.noreply.github.com>
2026-03-18 12:46:52 +0900

    [Silver II] Title: 과일 탕후루, Time: 16 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.cc
@@ -1,53 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt = 1, lo, hi, ans, num1, num2;
-int arr[200001], ncnt[10];
+int n, ans;
+int arr[200005], ncnt[10];
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
   cin >> n;
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < n; i++) {
     cin >> arr[i];
-
-  num1 = arr[0];
-  ncnt[num1]++;
-  while (hi < n - 1) {
-    if (arr[hi + 1] == num1) {
-      ncnt[num1]++;
-      hi++;
-      continue;
-    }
-    if (arr[hi + 1] == num2) {
-      ncnt[num2]++;
-      hi++;
-      continue;
-    }
-    if (cnt == 1) {
-      hi++;
-      num2 = arr[hi];
-      ncnt[num2]++;
-      cnt++;
-      continue;
-    }
-
-    ans = max(ans, hi - lo + 1);
-    while (lo < hi) {
-      lo++;
-      (arr[lo - 1] == num1) ? (ncnt[num1]--) : (ncnt[num2]--);
-
-      if (ncnt[num2] == 0) {
-        num2 = 0;
-        break;
-      } else if (ncnt[num1] == 0) {
-        num1 = num2;
-        num2 = 0;
-        break;
-      }
-    }
-    cnt--;
   }
-  ans = max(ans, hi - lo + 1);
+
+  int kind = 0;
+  for (int l = 0, r = 0; r < n; r++) {
+    if (ncnt[arr[r]]++ == 0) {
+      kind++;
+    }
+    while (kind > 2) {
+      if (--ncnt[arr[l]] == 0) {
+        kind--;
+      }
+      l++;
+    }
+    ans = max(ans, r - l + 1);
+  }
+
   cout << ans;
 }
