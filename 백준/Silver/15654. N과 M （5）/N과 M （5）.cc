cbc872d1508cb4f385bfbf9548062ec119cd546f
정선웅 <96913791+7bellaa@users.noreply.github.com>
2026-03-18 14:33:44 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -1,38 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, arr[9];
+int n, m, arr[9], ans[9];
 bool vis[9];
-vector<int> ans;
 
-void print_ans() {
-  for (auto i : ans)
-    cout << i << ' ';
-  cout << '\n';
-}
-
-void f(int x) {
-  if (x == m) {
-    print_ans();
+void f(int k) {
+  if (k == m) {
+    for (int i = 0; i < m; i++)
+      cout << ans[i] << ' ';
+    cout << '\n';
     return;
   }
-  for (int i = 1; i <= n; i++) {
-    if (vis[i] || arr[i] == arr[i - 1])
+
+  for (int i = 0; i < n; i++) {
+    if (vis[i])
       continue;
     vis[i] = true;
-    ans.push_back(arr[i]);
-    f(x + 1);
+    ans[k] = arr[i];
+    f(k + 1);
     vis[i] = false;
-    ans.pop_back();
   }
 }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
   cin >> n >> m;
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
     cin >> arr[i];
-  sort(arr + 1, arr + n + 1);
+
+  sort(arr, arr + n);
   f(0);
 }
