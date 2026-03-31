10a9535cb6a764cd7d190ac7b839e8c35885e5d7
정선웅 <96913791+7bellaa@users.noreply.github.com>
2026-03-31 11:41:44 +0900

    [Gold III] Title: 시로코와 은행털기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.cc
+++ 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool dp[81][16001];  // dp[i][w]: i명 뽑았을때 A_i의 합이 w가 될수있는가
+bitset<16001> dp[81];  // dp[i][w]: i명 뽑았을때 A_i의 합이 w가 될수있는가
 
 int main() {
   ios::sync_with_stdio(false);
@@ -9,23 +9,29 @@ int main() {
 
   int N, K, X;
   cin >> N >> K >> X;
-  dp[0][0] = true;
+  dp[0][0] = 1;
 
   for (int i = 0; i < N; i++) {
     int a, b;
     cin >> a >> b;
 
     for (int k = K; k >= 1; k--) {
-      for (int w = K * X; w >= a; w--) {
-        if (dp[k - 1][w - a]) dp[k][w] = true;
-      }
+      dp[k] |= dp[k - 1] << a;
     }
   }
 
-  int mx = 0;
-  for (int w = 0; w <= 16000; w++) {
+  // int mx = 0;
+  int ans = 0;
+  for (int w = 0; w <= K * X; w++) {
     if (!dp[K][w]) continue;
-    if (abs((double)K * X / 2 - w) < abs((double)K * X / 2 - mx)) mx = w;
+    // if (abs((double)K * X / 2 - w) < abs((double)K * X / 2 - mx)) mx = w;
+    // double 형변환하기 싫어서 전체에 2를 곱함
+    // if (abs(K * X - 2 * w) < abs(K * X - 2 * mx)) mx = w;
+
+    // 원래 우리가 찾으려는 w는 w * (K*X - w)가 최대가 되는 w이다.
+    // 그래서 그냥 저걸 그대로 계산해서 저게 최대가 될때마다 mx 갱신해주면 된다.
+    // ** 이걸 더 추천 **
+    ans = max(ans, w * (K * X - w));
   }
-  cout << mx * (K * X - mx);
+  cout << ans;
 }
